d11a77fd3902d4dc3c9aebbc99c64929c213b06a
yosssi <yoshida.keiji.84@gmail.com>
2014-02-27 10:35:06 +0900

    Updated some modules.

--- app/models/application.go
+++ app/models/application.go
@@ -1,19 +1,37 @@
 package models
 
+import (
+	"github.com/yosssi/gologger"
+	"strconv"
+)
+
+// An Application represents an application context.
 type Application struct {
 	ServerConfig *ServerConfig
 	LoggerConfig *LoggerConfig
+	Logger       *gologger.Logger
 }
 
+// PortString returns a string value of ServerConfig's Port.
+func (a *Application) PortString() string {
+	return strconv.Itoa(a.ServerConfig.Port)
+}
+
+// NewApplication generates an Application and returns it.
 func NewApplication() (*Application, error) {
 	serverConfig, err := NewServerConfig()
 	if err != nil {
 		return nil, err
 	}
+
 	loggerConfig, err := NewLoggerConfig()
 	if err != nil {
 		return nil, err
 	}
-	app := &Application{ServerConfig: serverConfig, LoggerConfig: loggerConfig}
+
+	logger := &gologger.Logger{Name: loggerConfig.Name, Level: loggerConfig.Level, File: loggerConfig.File}
+
+	app := &Application{ServerConfig: serverConfig, LoggerConfig: loggerConfig, Logger: logger}
+
 	return app, nil
 }
--- main.go
+++ main.go
@@ -3,22 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/yosssi/xpress/app/models"
+	"net/http"
 )
 
 var (
-	application models.Application
+	application *models.Application
 )
 
 // init executes initial processes.
 func init() {
-	application, err := models.NewApplication()
+	var err error
+	application, err = models.NewApplication()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(application)
 }
 
 // main executes main processes.
 func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+application.PortString(), nil)
+}
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%+v\n", application)
 }
