5be55f639f7f1f8beb8eab2a94458aa2491e7949
yosssi <yoshida.keiji.84@gmail.com>
2014-03-04 22:27:50 +0900

    Updated main.go.

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/yosssi/xpress/app/models"
 	"github.com/yosssi/xpress/app/routes"
-	"net/http"
 )
 
 // main executes main processes.
 func main() {
 	app, err := models.NewApplication()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	routes.Routes(app)
