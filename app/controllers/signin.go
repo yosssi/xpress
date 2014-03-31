191f517b238fbc95d3a9b0383dfffbdc398edb79
yosssi <yoshida.keiji.84@gmail.com>
2014-03-31 09:41:15 +0000

    Updated files.

--- app/controllers/signin.go
+++ app/controllers/signin.go
@@ -1,10 +1,10 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
 
+	"github.com/yosssi/xpress/app/consts"
 	"github.com/yosssi/xpress/app/models"
 )
 
@@ -13,19 +13,43 @@ func SigninIndex(w http.ResponseWriter, r *http.Request, app *models.Application
 }
 
 func SigninCallback(w http.ResponseWriter, r *http.Request, app *models.Application) {
+	// parse the URL.
 	u, err := url.Parse(r.URL.String())
+
 	if err != nil {
 		handleError(w, r, app, err)
+		return
 	}
+
+	// get a GitHub access token.
 	accessToken, err := app.GitHubClient.GetAccessToken(u.Query().Get("code"))
+
 	if err != nil {
 		handleError(w, r, app, err)
+		return
 	}
-	app.Logger.Debug(fmt.Sprintf("accessToken: %s", accessToken))
+
+	app.Logger.Debugf("accessToken: %s", accessToken)
+
+	// get a GitHub user.
 	user, err := app.GitHubClient.GetAuthenticatedUser(accessToken)
+
 	if err != nil {
 		handleError(w, r, app, err)
+		return
 	}
-	app.Logger.Debug(fmt.Sprintf("user: %+v", user))
+
+	app.Logger.Debugf("user: %+v", user)
+
+	// get an account.
+	code, account, err := app.ElasticsearchClient.Get(consts.ElasticsearchIndexXpress, consts.ElasticsearchTypeAccount, consts.ElasticsearchAccountTopID)
+
+	if err != nil {
+		handleError(w, r, app, err)
+		return
+	}
+
+	app.Logger.Debugf("code: %d, account: %+v", code, account)
+
 	render("./app/views/signin/index.gold", nil, w, r, app)
 }
